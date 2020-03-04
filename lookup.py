45ab42827954075195bf21d7507d2f7aad18299a
yuyi5453 <744530689@qq.com>
2020-03-04 09:49:01 +0800

    lookup

--- lookup.py
+++ lookup.py
@@ -1,5 +1,13 @@
 # coding=gbk
 
+#规范化检查
+def check(a,b,c,d):
+    if len(a)!=len(b):
+        return 1
+    elif len(c)!=len(d):
+        return 2
+    return 0
+
 #对一个区域进行标准化处理
 #全部转换为字符串，并且去空格
 def std_handle(rng):
@@ -26,6 +34,49 @@ def lookup(wb,A,B,C,D):
     std_handle(C_rng)
     D_rng=list()
 
+    err = check(A_rng, B_rng, C_rng, D_rng)
+    if err == 1:
+        print('A,B区域大小不一致')
+        return
+    elif err == 2:
+        print('C,D大小不一致')
+        return
+
+    for c in C_rng:
+            flag=0
+            for i in range(0,len(A_rng)):
+                a = A_rng[i]
+                if c==a:
+                    flag=1
+                    D_rng.append(B_rng[i])
+                    break
+
+            if flag==0:
+                D_rng.append('None')
+
+    sht.range(D).options(transpose=True).value=D_rng
+    return
+
+#分表匹配  用于AB和CD不在同一个文件中
+def lookup(wb1,wb2,A,B,C,D):
+    sht1=wb1.sheets[0]
+    sht2=wb2.sheets[0]
+    A_rng = sht1.range(A).expand('down').value
+    std_handle(A_rng)
+    B_rng = sht1.range(B).expand('down').value
+    std_handle(B_rng)
+    C_rng = sht2.range(C).expand('down').value
+    std_handle(C_rng)
+    D_rng = list()
+
+    err=check(A_rng,B_rng,C_rng,D_rng)
+    if err==1:
+        print('A,B区域大小不一致')
+        return
+    elif err==2:
+        print('C,D大小不一致')
+        return
+
     for c in C_rng:
         flag=0
         for i in range(0,len(A_rng)):
@@ -38,9 +89,8 @@ def lookup(wb,A,B,C,D):
         if flag==0:
             D_rng.append('None')
 
-    sht.range(D).options(transpose=True).value=D_rng
+    sht2.range(D).options(transpose=True).value = D_rng
     return
-
 import xlwings as xl
 try:
     app = xl.App(visible=False)
